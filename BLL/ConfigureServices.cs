65b44f2fdd8b68b0a17121e7d8341cc2d86d7667
Filip Mossberg <filip.mossberg@gmail.com>
2024-05-13 15:04:34 +0200

    Add project files.

---
+++ BLL/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BLL
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection Configure(
+            this IServiceCollection service)
+        {
+
+
+            return service;
+        }
+    }
+}
